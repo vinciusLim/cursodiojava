22dc9bd4af6ee4141948c751c817b6406ffd8d9a
vinciusLim <vinicius2008852@gmail.com>
2024-04-29 19:06:41 -0300

    primeiro commit

---
+++ ContaTerminal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digitar numero");
+        int numero = scanner.nextInt();
+        System.out.println("Digitar agencia");
+        String agencia = scanner.next();
+        System.out.println("Digitar nome do cliente");
+        String nomeCliente = scanner.next();
+        System.out.println("Digitar saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
